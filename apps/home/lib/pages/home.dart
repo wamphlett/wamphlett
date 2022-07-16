bb939992d97335f1d9b98ef88c3281f4492d8354
Warren Amphlett <warren@bugsnag.com>
2022-07-16 12:29:06 +0100

    fix hover issue and update things I play with

--- apps/home/lib/pages/home.dart
+++ apps/home/lib/pages/home.dart
@@ -10,98 +10,103 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double textMultiplier = MediaQuery.of(context).size.width > 500
-      ? MediaQuery.of(context).size.width > 800 ? 1 : 0.7
-      : 0.5;
+        ? MediaQuery.of(context).size.width > 800
+            ? 1
+            : 0.7
+        : 0.5;
 
     double nameSize = 100 * textMultiplier;
 
     return PageLayout(
       child: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 100),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              
-              // Name
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("WARREN", style: TextStyle(
-                    fontSize: nameSize,
-                    height: 1,
-                    fontWeight: FontWeight.w900
-                  )),
-                  RichText(
-                    text: TextSpan(
-                      style: TextStyle(
-                        color: Colors.black,
-                        height: .9
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(text: 'AMPHLETT', style: TextStyle(
-                          fontSize: nameSize,
-                          height: .9,
-                          fontFamily: 'Heebo',
-                          fontWeight: FontWeight.w900
-                        )),
-                        TextSpan(text: '.', style: TextStyle(
+          child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 100),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            // Name
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("WARREN",
+                    style: TextStyle(
                         fontSize: nameSize,
-                          height: .9,
-                          fontFamily: 'Heebo',
-                          fontWeight: FontWeight.w900,
-                          color: BrandColors.primary
-                        )),
-                      ],
-                    ),
+                        height: 1,
+                        fontWeight: FontWeight.w900)),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(color: Colors.black, height: .9),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'AMPHLETT',
+                          style: TextStyle(
+                              fontSize: nameSize,
+                              height: .9,
+                              fontFamily: 'Heebo',
+                              fontWeight: FontWeight.w900)),
+                      TextSpan(
+                          text: '.',
+                          style: TextStyle(
+                              fontSize: nameSize,
+                              height: .9,
+                              fontFamily: 'Heebo',
+                              fontWeight: FontWeight.w900,
+                              color: BrandColors.primary)),
+                    ],
                   ),
-                  // Tagline
-                  Text("web developer and tech addict", style: TextStyle(
-                    fontSize: 40 * textMultiplier,
-                    height: .8,
-                    fontWeight: FontWeight.w100
-                  )),
-                ],
-              ),
+                ),
+                // Tagline
+                Text("software engineer and tech addict",
+                    style: TextStyle(
+                        fontSize: 37 * textMultiplier,
+                        height: .8,
+                        fontWeight: FontWeight.w100)),
+              ],
+            ),
 
-              Column(
-                children: <Widget>[
-                  // Skills
-                  Container(
-                    child: Column(
-                      children: <Widget>[
-                        Text("things I play with", style: TextStyle(
-                          fontSize: 24 * textMultiplier,
-                          fontWeight: FontWeight.w100
-                        )),
-                        ShowUp(child: Text("GO PHP LARAVEL FLUTTER", style: TextStyle(
-                          fontWeight: FontWeight.w900,
-                          fontSize: 32 * textMultiplier
-                        ))),
-                        TextReel(
-                          children: [
-                            ExtraText("kubernetes docker unix", fontSize: 16 * textMultiplier),
-                            ExtraText("MySQL mongoDB", fontSize: 16 * textMultiplier),
-                            ExtraText("redis memcached", fontSize: 16 * textMultiplier),
-                            ExtraText("nodejs react css sass", fontSize: 16 * textMultiplier),
-                            ExtraText("GCP AWS", fontSize: 16 * textMultiplier),
-                          ],
-                        )
-                      ],
-                    ),
+            Column(
+              children: <Widget>[
+                // Skills
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Text("things I play with",
+                          style: TextStyle(
+                              fontSize: 24 * textMultiplier,
+                              fontWeight: FontWeight.w100)),
+                      ShowUp(
+                          child: Text("GO PHP LARAVEL FLUTTER",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 32 * textMultiplier))),
+                      TextReel(
+                        children: [
+                          ExtraText("kubernetes docker unix",
+                              fontSize: 16 * textMultiplier),
+                          ExtraText("gRPC rabbitMQ",
+                              fontSize: 16 * textMultiplier),
+                          ExtraText("MySQL mongoDB",
+                              fontSize: 16 * textMultiplier),
+                          ExtraText("redis memcached",
+                              fontSize: 16 * textMultiplier),
+                          ExtraText("nodejs react css sass",
+                              fontSize: 16 * textMultiplier),
+                          ExtraText("GCP AWS", fontSize: 16 * textMultiplier),
+                        ],
+                      )
+                    ],
                   ),
+                ),
 
-                  // Social
-                  SocialIcons()
-                ],
-              )
-
-            ],
-          ),
-        )
-      ),
+                // Social
+                SocialIcons()
+              ],
+            )
+          ],
+        ),
+      )),
     );
   }
 }
@@ -112,9 +117,7 @@ class ExtraText extends StatelessWidget {
   ExtraText(this.text, {@required this.fontSize});
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: TextStyle(
-      fontSize: fontSize,
-      fontWeight: FontWeight.w100
-    ));
+    return Text(text,
+        style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.w100));
   }
 }
