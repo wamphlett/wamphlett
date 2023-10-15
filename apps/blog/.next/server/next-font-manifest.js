a6fc23750e915ab54bc5a6e6b08c1edc52be2300
Warren Amphlett <warrenamphlett@gmail.com>
2023-10-16 00:49:31 +0100

    remove the .next dir

--- apps/blog/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/warren.amphlett/dev/wamphlett/blog-site/src/app/layout\":[\"static/media/25e11f223a13018e-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
