cf0c52c128f224a8545e7bd65206740f6a9cdb69
Warren Amphlett <warrenamphlett@gmail.com>
2026-01-02 17:15:36 +0000

    add GTM and update metadata

--- apps/rtk/src/app/layout.tsx
+++ apps/rtk/src/app/layout.tsx
@@ -9,20 +9,20 @@ const heebo = Heebo({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://warrenamphlett.co.uk'),
+  metadataBase: new URL('https://rtk.warrenamphlett.co.uk'),
   title: {
     template: '%s | Warren Amphlett',
     default: 'Warren Amphlett',
   },
-  description: 'Software engineer and Photographer',
+  description: 'Remembering the Kanji - How I remembered the Kanji.',
   authors: { name: 'Warren Amphlett' },
   openGraph: {
     type: 'website',
     locale: 'en_GB',
-    url: 'https://warrenamphlett.co.uk',
+    url: 'https://rtk.warrenamphlett.co.uk',
     images:
-      'https://library.wamphlett.net/photos/website/2023/albania/three-of-a-kind.jpg',
-    siteName: 'Warren Amphlett',
+      'https://library.wamphlett.net/photos/website/2025/remembering-the-kanji.jpg',
+    siteName: 'Remembering the Kanji',
   },
 };
 
@@ -34,16 +34,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={heebo.className}>
-        {/* <Script src="https://www.googletagmanager.com/gtag/js?id=" /> */}
-        <Script id="google-analytics">
-          {/* {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          //gtag('config', 'G-WB51J1WT4Q');
-        `} */}
-        </Script>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5D5DFD8S');</script>
+        
         {children}
       </body>
     </html>
