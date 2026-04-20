840096ee680180cffe2f914d6d96f59678406929
Warren Amphlett <warrenamphlett@gmail.com>
2026-04-20 15:29:01 +0100

    remove old config

--- apps/home/src/app/images/2024japansummer.tsx
+++
@@ -1,9 +0,0 @@
-import { buildImages, libraryUrl } from '../data';
-
-export default buildImages(libraryUrl('2024/japansummer'), {
-  near_perfect: {
-    title: 'Hidden beach in Izu',
-    description: 'A day of sun, sea and snorkeling',
-    url: 'me_and_waka.jpg',
-  },
-});
