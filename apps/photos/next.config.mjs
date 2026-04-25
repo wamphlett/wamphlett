acf06532bb8bb5da486f665bc2124620aa668115
Warren Amphlett <warrenamphlett@gmail.com>
2026-04-25 16:19:35 +0100

    fix dockerfile issue

--- apps/photos/next.config.mjs
+++ apps/photos/next.config.mjs
@@ -2,6 +2,7 @@ import withPlaiceholder from "@plaiceholder/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     deviceSizes: [640, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
