e0de7182426a5e469d402f1d4a1cadf3cf292b3f
Warren Amphlett <warrenamphlett@gmail.com>
2026-04-25 16:10:35 +0100

    update the dockerfile

--- apps/photos/src/app/manage/login/page.tsx
+++ apps/photos/src/app/manage/login/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useState, FormEvent } from 'react';
 import { useRouter } from 'next/navigation';
 
--- apps/photos/src/app/manage/page.tsx
+++ apps/photos/src/app/manage/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useState, useRef, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 import PhotoModal from './PhotoModal';
